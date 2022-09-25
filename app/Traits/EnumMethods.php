c3d55c95da38082d07bae5047f687f6f57c46ac8
Rafael Arend <rafinhaarend123@hotmail.com>
2022-09-25 18:09:56 -0400

    Melhora EnumMethods para que seja possível apartir de qualquer enum verificar se o enum atual é alguma propriedade de self enum. Ex: Status::ACTIVE->isActive //true

--- app/Traits/EnumMethods.php
+++ app/Traits/EnumMethods.php
@@ -2,6 +2,10 @@
 
 namespace App\Traits;
 
+use App\Enums\Common\Status;
+use Error;
+use ReflectionEnum;
+
 trait EnumMethods
 {
     public static function all(): \Illuminate\Support\Collection
@@ -30,5 +34,17 @@ trait EnumMethods
     {
         $all = self::all();
         return self::tryFrom($all->search($label));
+    }
+
+    public function __call($name, $arguments)
+    {
+        strtolower(substr($name, 0, 2)) === 'is' or throw new Error("method doesnt exist", 1);
+        $enum = strtoupper(substr($name, 2));
+        $class = $this::class;
+
+        $reflection = new ReflectionEnum(Status::class);
+        $enum = $reflection->hasConstant($enum) ? $reflection->getConstant($enum) : throw new Error("'$enum' is not a valid backing value for enum '$class'", 1);
+
+        return $this->value === $enum->value ?? false;
     }
 }
