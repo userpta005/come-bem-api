1e0e618389a37c5e6495e8f55cea567d3ad151bc
Rafael Arend <rafinhaarend123@hotmail.com>
2022-10-05 15:28:39 -0400

    Atualiza Laravel

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,17 +1,1 @@
-/**
- * Next, we will create a fresh React component instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-import { createApp } from 'vue';
-import App from './app.vue'
-
-// Import our custom CSS
-import '../sass/app.scss'
-
-// Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
-
-const app = createApp(App)
-app.mount("#app")
+import './bootstrap';
--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue'
-
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-import Icons from 'unplugin-icons/vite'
 
 export default defineConfig({
     plugins: [
-        vue(),
         laravel({
-            input: 'resources/js/app.js',
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        AutoImport({
-            resolvers: [ElementPlusResolver()],
-        }),
-        Components({
-            resolvers: [ElementPlusResolver()],
-        }),
-        Icons(),
     ],
-    resolve: {
-        alias: {
-            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-        }
-    },
 });
