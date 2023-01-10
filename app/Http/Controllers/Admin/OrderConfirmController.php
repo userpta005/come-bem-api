054b53ac725ac9ab7b8c1a7a423975b030c07917
Claysllan Xavier <claysllan@gmail.com>
2023-01-10 14:56:19 -0300

    :hammer: Valida estoque do pedido na retirada

--- app/Http/Controllers/Admin/OrderConfirmController.php
+++ app/Http/Controllers/Admin/OrderConfirmController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Admin;
 use App\Enums\OrderStatus;
 use App\Http\Controllers\Controller;
 use App\Models\Order;
+use App\Models\Stock;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class OrderConfirmController extends Controller
 {
@@ -17,16 +19,31 @@ class OrderConfirmController extends Controller
      */
     public function __invoke(Request $request, $id)
     {
-        $order = Order::findOrFail($id);
+        $order = Order::with('orderItems.product.stock')->findOrFail($id);
 
         if ($order->status == OrderStatus::RETIRED) {
             return redirect()->to('home')
-                ->withError( "Pedido já retirado");
+                ->withError("Pedido já retirado");
         }
 
+        foreach ($order->orderItems as $item) {
+            if ($item->quantity > $item->product->stock->quantity) {
+                return redirect()->to('home')
+                    ->withError("{$item->product->name} sem quantidade no estoque");
+            }
+        }
+
+        DB::beginTransaction();
         $order->delivery_date = now();
         $order->status = OrderStatus::RETIRED;
         $order->save();
+        foreach ($order->orderItems as $item) {
+            if ($item->quantity > $item->product->stock->quantity) {
+                Stock::where('id', $item->product->stock->id)
+                    ->decrement('quantity', $item->quantity);
+            }
+        }
+        DB::commit();
 
         return redirect()->to('home')
             ->withStatus("Pedido retirado com sucesso.");
