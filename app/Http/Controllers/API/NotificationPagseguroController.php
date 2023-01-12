882b8347da7d4a072f90afd8d9535b28f3daed82
Claysllan Xavier <claysllan@gmail.com>
2023-01-12 15:20:32 -0300

    :hammer: Corrige controller da notificação

--- app/Http/Controllers/API/NotificationPagseguroController.php
+++ app/Http/Controllers/API/NotificationPagseguroController.php
@@ -25,53 +25,14 @@ class NotificationPagseguroController extends Controller
      */
     public function __invoke(Request $request, $id)
     {
-        Log::info("NOTIFICACAO: ".json_encode($request->all()));
-        $code = $request->input('notificationCode');
-        $type = $request->input('notificationType');
 
-
-        if (empty($code) || empty($type)) {
-            return;
-        }
-
-        $isSandbox = boolval(config('laravel-pagseguro.use-sandbox'));
-
-        $url = config('laravel-pagseguro.notificacation.production');
-
-        if ($isSandbox) {
-            $url = config('laravel-pagseguro.notificacation.sandbox');
-        }
-
-        $store = Store::with('tenant')->find($id);
-
-        if (!$store) {
-            return;
-        }
-
-        $credentials = "email={$store->tenant->pagseguro_email}&token={$store->tenant->pagseguro_token}";
-
-        $response = Http::withHeaders([
-            'Content-Type' => 'application/json',
-            'x-api-version' => '4.0'
-        ])->get(
-            "$url/{$code}?{$credentials}",
-        );
-
-        if ($response->failed()) {
-            throw new Exception($response->body());
-        }
-
-        $xml = simplexml_load_string($response->body(), "SimpleXMLElement", LIBXML_NOCDATA);
-        $json = json_encode($xml);
-        $array = json_decode($json, TRUE);
-
-        $creditPurchase = CreditPurchase::where('uuid', $array['reference'])->first();
+        $creditPurchase = CreditPurchase::where('uuid', $request->reference_id)->first();
         DB::beginTransaction();
         $inputs = [];
         if ($creditPurchase) {
             $inputs['status'] = CreditPurchaseStatus::PENDING;
 
-            if ($array['status'] == 3) {
+            if ($request->charges[0]->status == "PAID") {
                 $inputs['status'] = CreditPurchaseStatus::PAYED;
 
                 $data = [];
@@ -84,9 +45,7 @@ class NotificationPagseguroController extends Controller
                 AccountEntry::query()->create($data);
 
                 Account::where('id', $creditPurchase->account_id)->increment('balance', $creditPurchase->amount);
-            }
-
-            if ($array['status'] == 7) {
+            } elseif ($request->charges[0]->status == "CANCELED") {
                 $inputs['status'] = CreditPurchaseStatus::CANCELED;
             }
 
