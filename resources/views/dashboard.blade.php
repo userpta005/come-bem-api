434fa695dae3b9d7c3e657135ebcbf9fbe2cb745
Rafael Arend <rafinhaarend123@hotmail.com>
2023-01-26 12:16:43 -0400

    Separa a lógics CSS e JS da Tabs

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,21 +5,6 @@
     .card {
       border-top-left-radius: 0px;
     }
-
-    .nav-item-custom {
-      background: rgba(0, 0, 0, 0.05);
-      border-radius: 10px 10px 0 0;
-      box-shadow: 0 1px 15px 0 rgba(0, 0, 0, 0.05);
-    }
-
-    .nav-link-custom {
-      color: black;
-      font-weight: 500;
-    }
-
-    .active-custom {
-      background: white;
-    }
   </style>
 @endpush
 
@@ -27,6 +12,24 @@
   <div class="row">
     <div class="col-md-12">
       <div>
+        @push('css')
+          <style>
+            .nav-item-custom {
+              background: rgba(0, 0, 0, 0.05);
+              border-radius: 10px 10px 0 0;
+              box-shadow: 0 1px 15px 0 rgba(0, 0, 0, 0.05);
+            }
+
+            .nav-link-custom {
+              color: black;
+              font-weight: 500;
+            }
+
+            .active-custom {
+              background: white;
+            }
+          </style>
+        @endpush
         <ul class="nav">
           <li class="nav-item nav-item-custom home">
             <a class="nav-link nav-link-custom"
@@ -37,19 +40,21 @@
               href="{{ route('pdv') }}">PDV/Pedidos</a>
           </li>
         </ul>
-        <script>
-          const currentUrl = window.location.href
-          const lastPart = currentUrl.match(/\/([^\/]+)$/)[1]
+        @push('js')
+          <script>
+            const currentUrl = window.location.href
+            const lastPart = currentUrl.match(/\/([^\/]+)$/)[1]
 
-          const home = document.querySelector('.home')
-          const pdv = document.querySelector('.pdv')
+            const home = document.querySelector('.home')
+            const pdv = document.querySelector('.pdv')
 
-          if (lastPart === 'home') {
-            home.classList.add('active-custom')
-          } else if (lastPart === 'pdv') {
-            pdv.classList.add('active-custom')
-          }
-        </script>
+            if (lastPart === 'home') {
+              home.classList.add('active-custom')
+            } else if (lastPart === 'pdv') {
+              pdv.classList.add('active-custom')
+            }
+          </script>
+        @endpush
       </div>
       <div class="card ">
         <div class="card-header">
